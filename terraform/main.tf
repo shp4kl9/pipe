e45acf7c33245a43cb77244e5d56f1d28f148d5e
shp4kl9 <bks201b@gmail.com>
2025-09-21 21:29:18 +0300

    Fix terraform err 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,6 @@ resource "null_resource" "build_and_run" {
   provisioner "local-exec" {
     command = "cd ../app && docker build -t flask-app:latest . && docker stop flask-app || true && docker rm flask-app || true && docker run -d -p 8000:5000 --name flask-app flask-app:latest"
   }
-  provisioner "local-exec"{
-    docker stop flask-app || true
- }
 
   provisioner "local-exec" {
     when    = destroy
